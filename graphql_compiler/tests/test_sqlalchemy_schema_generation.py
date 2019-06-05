f7d05160fa9bbdc9fee5b5cfb9fc63a5af15184e
pmantica1 <manticapedro@gmail.com>
2019-06-05 16:31:17 -0400

    Add MSSQL types (#341)
    * Add MSSQL types
    
    * Fix test typo
    
    * Add missing copyright text
    
    * Nit
    
    * Add TODO

--- graphql_compiler/tests/test_sqlalchemy_schema_generation.py
+++ graphql_compiler/tests/test_sqlalchemy_schema_generation.py
@@ -1,18 +1,18 @@
 # Copyright 2019-present Kensho Technologies, LLC.
 import unittest
 
-from graphql.type import GraphQLString
+from graphql.type import GraphQLInt, GraphQLString
 from graphql.utils.schema_printer import print_schema
 import pytest
 from sqlalchemy import Column, MetaData, Table
+from sqlalchemy.dialects.mssql.base import TINYINT
 from sqlalchemy.types import Binary, Integer, String
 
 from graphql_compiler import get_graphql_schema_from_sqlalchemy_metadata
 
 from ..schema_generation.schema_graph import VertexType
-from ..schema_generation.sqlalchemy.schema_graph_builder import (
-    _try_get_graphql_scalar_type, get_sqlalchemy_schema_graph
-)
+from ..schema_generation.sqlalchemy.scalar_type_mapper import try_get_graphql_scalar_type
+from ..schema_generation.sqlalchemy.schema_graph_builder import get_sqlalchemy_schema_graph
 from .test_helpers import compare_schema_texts
 
 
@@ -25,7 +25,8 @@ def _get_sql_metadata():
         metadata,
         Column('supported_type', String()),
         Column('non_supported_type', Binary()),
-        Column('default', Integer(), default=42)
+        Column('default', Integer(), default=42),
+        Column('column_with_mssql_type', TINYINT),
     )
 
     return metadata
@@ -51,7 +52,7 @@ class SQLALchemyGraphqlSchemaGenerationTests(unittest.TestCase):
 
     def test_warn_when_type_is_not_supported(self):
         with pytest.warns(Warning):
-            _try_get_graphql_scalar_type('binary', Binary)
+            try_get_graphql_scalar_type('binary', Binary)
 
     def test_default_value_parsing(self):
         a_vertex = self.schema_graph.get_element_by_class_name('A')
@@ -61,6 +62,10 @@ class SQLALchemyGraphqlSchemaGenerationTests(unittest.TestCase):
         # Tests that a class is in its own subclass/superclass set.
         self.assertEqual(self.schema_graph.get_subclass_set('A'), {'A'})
         self.assertEqual(self.schema_graph.get_superclass_set('A'), {'A'})
+
+    def test_mssql_type_mapping(self):
+        a_vertex = self.schema_graph.get_element_by_class_name('A')
+        self.assertEqual(a_vertex.properties['column_with_mssql_type'].type, GraphQLInt)
 
     def test_sqlalchemy_graphql_schema_generation(self):
         expected_schema_text = '''
@@ -84,6 +89,7 @@ class SQLALchemyGraphqlSchemaGenerationTests(unittest.TestCase):
 
             type A {
                 _x_count: Int
+                column_with_mssql_type: Int
                 default: Int
                 supported_type: String
             }
